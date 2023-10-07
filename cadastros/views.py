e7006cc0becc4ecc9ffd9988559f1fb05e97fdd4
poliana-dev <m.polisys02@gmail.com>
2023-10-07 10:18:16 -0300

    funçoes update/delete na views de cadastros

--- cadastros/views.py
+++ cadastros/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from . import forms
 from . import models
 
+from .forms import PostForm
+from django.shortcuts import get_object_or_404, render
+from .models import Postagens
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 # Criar Posts
 def post_create(request):
     if request.method == "POST":
@@ -20,4 +27,29 @@ def posts_list(request):
 
     return render(request, "listas/posts_assistencia_social.html", {'posts': postagens})
 
+# update post
+
+def post_update(request, id):
+    postagens = get_object_or_404(Postagens, id=id) #verifica se é o mesmo id
+    form = PostForm(request.POST or None, request.FILES or None, instance=postagens)
+    if request.method =='POST':
+        if form.is_valid(): #se for valido, salva o form e retorna a mensagem e a atualização do form
+            form.save()
+
+            messages.success(request, "A postagem foi atualizada com sucesso!")
+            return HttpResponseRedirect(reverse('post_list', args=[postagens.id]))
+        
+    return render(request, "post_form.html", {"form":form})
+
+# delete post
+
+def post_delete(request, id):
+    postagens = Postagens.objects.get(id=id) #verifica se é o mesmo id
+    if request.method == 'POST':
+        postagens.delete()
+        messages.success(request, 'A postagem foi deletada com sucesso!')
+        return HttpResponseRedirect(reverse("post_list"))
+    
+    # esse html ainda não existe
+    return render(request, 'post_delete.html')
 
